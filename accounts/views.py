1aa823e4b9df417d9ef6a2120a38a356255c8a5c
leonardothiesen <127902767+leonardothiesen@users.noreply.github.com>
2023-11-08 21:15:52 -0300

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
 
 def user_login(request):
-    return render(request,'pages/login.html')
+
+    if request.method == 'POST':
+        user = request.POST.get('username')
+        password = request.POST.get('password')
+
+        userVerify = auth.authenticate(
+            request, username=user, password=password)
+
+        if userVerify == None:
+            messages.info(request, 'Usuário ou senha incorretos')
+            return redirect('login')
+        else:
+            auth.login(request, userVerify)
+            return redirect('home')
+    
+    else:  
+        return render(request,'pages/login.html')
 
